4e668ba2dc59d0245247b4d1150f307e8c723145
Eivy <modern.times.rock.and.roll+git@gmail.com>
2021-03-21 16:29:06 +0900

    Fix resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,15 +2,15 @@ package graph
 
 import "github.com/eivy/aptitude_bulb/graph/model"
 
-//go:generate go run github.com/99designs/gqlgen
+//go:generate go run github.com/99designs/gqlgen generate
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	Users []*model.User
-	Items []*model.Item
+	Users []model.User
+	Items []model.Item
 	User  *model.User
 	Item  *model.Item
 }
